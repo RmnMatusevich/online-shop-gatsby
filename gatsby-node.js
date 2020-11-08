1d866cbf53155f31958054ccb48604e558b55e9c
Roman Matusevich <rmnmatusevich@gmail.com>
2020-11-08 14:46:13 +0300

    Add ref data

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require('path');
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
-  const blogPostTemplate = path.resolve('src/templates/blogTemplate.js');
+  const blogPostTemplate = path.resolve('src/pages/Components/Components');
 
   return graphql(`
     {
