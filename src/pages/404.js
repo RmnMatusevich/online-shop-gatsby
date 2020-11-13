e9426c4d4f130496d996c667e33bda14cd4de150
Roman Matusevich <rmnmatusevich@gmail.com>
2020-11-14 00:20:58 +0300

    Customize 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,48 @@
 import React from "react";
+import { Helmet } from "react-helmet"
+import Header from "../components/Header/Header"
+import HeaderLinks from "../components/Header/HeaderLinks"
+import Parallax from "../components/Parallax/Parallax"
+import GridContainer from "../components/Grid/GridContainer"
+import GridItem from "../components/Grid/GridItem"
 
-const Index = () => {
+const Index = (props) => {
+  const { classes, data, ...rest } = props
+
   return (
-    <>
-      <h1>PAGE 404</h1>
-      <h1>NOT FOUND</h1>
-      <h1>PAGE 404</h1>
-      <h1>NOT FOUND</h1>
-      <h1>PAGE 404</h1>
-      <h1>NOT FOUND</h1>
-      <h1>PAGE 404</h1>
-      <h1>NOT FOUND</h1>
-    </>
+    <div>
+      <Helmet>
+        <title>Tech Tag</title>
+        <meta
+          name="description"
+          content="Быстрый и качественный ремонт телефонов, ремонт планшетов, ремонт iqos,ремонт часов, ремонт ноутбуков"
+        />
+        <meta charSet="utf-8" />
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <Header
+        rightLinks={<HeaderLinks />}
+        fixed
+        color="white"
+        changeColorOnScroll={{
+          height: 400,
+          color: "white",
+        }}
+        {...rest}
+      />
+      <Parallax image={require("../../static/img/backgroundmain.svg")}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem>
+              <div className={classes.brand}>
+                <h1 className={classes.title}>Tech Tag</h1>
+                <h3 className={classes.subtitle}>Look into the future</h3>
+              </div>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+    </div>
   )
 }
 
